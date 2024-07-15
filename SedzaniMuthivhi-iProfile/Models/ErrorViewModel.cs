b93e74690b1c9d349c72a9cb4cc9205deb6bcf54
Sedzani <Sedzani@SEDZ>
2024-07-15 21:51:09 +0200

    Add project files.

---
+++ SedzaniMuthivhi-iProfile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SedzaniMuthivhi_iProfile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
